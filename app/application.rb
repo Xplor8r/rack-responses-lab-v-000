05b5791edf5e9b3dc0027ab3743072a299bebe0d
Michael Pieschel <sixtyhectares@yahoo.com>
2018-10-02 15:02:49 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@ class Application
   
   def call(env)
     resp = Rack::Response.new
-    resp.write "Good Morning!" 
+    resp.write "Good Morning!" if Time =
     
     resp.write "Good Afternoon!" 
     resp.finish
